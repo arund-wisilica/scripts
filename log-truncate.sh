9fd01976a89e118ecb9f74ad0f987939fc68538e
arund-wisilica <41982652+arund-wisilica@users.noreply.github.com>
2020-02-10 14:48:17 +0530

    Create log-truncate.sh

---
+++ log-truncate.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+
+#delete older than 7 days of logs
+#find  /var/www/html/wiseconnect/api/data/logs/* -ctime +7 -type d -exec rm -r {} \;
+
+#truncate logs with size greater than 1GB
+list=$(du -sch $(find /var/www/html/wiseconnect/api/data/logs/ -type f -size +999M -follow -print) | sort -n -r | head -n 5 | grep -v "total" | grep -v ".")
+if [ "$list" = "" ]; then
+exit
+fi
+
+for j in "${list[@]}";do
+ fname=$(printf '%s\n' "${list[@]}" | awk '{print $2}')
+pwd
+echo $fname
+#> $fname
+done
